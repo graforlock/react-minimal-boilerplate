9c0e28e1ae0784f253f11852d67f284662d04129
Maciej Sitko <maciej@neatcube.co.uk>
2016-04-17 21:21:01 +0100

    updated package json

--- src/services/httpservice.js
+++
@@ -1,9 +0,0 @@
-import Fetch from 'whatwg-fetch';
-const baseUrl = 'http://localhost:8080';
-
-const service = {
-  get: (url) => fetch(baseUrl + url).then(response => response.json()),
-  post: (url) => {}
-};
-
-export default service;
